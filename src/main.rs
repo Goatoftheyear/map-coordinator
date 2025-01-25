cb60e306d6f397da8dac86d3f64c90c4c02e3369
px <lim.px100@gmail.com>
2025-01-25 23:22:32 +0800

    add create location feature

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use std::io;
+use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello!");
+    let mut opened_maps:HashMap<String, String> = HashMap::new();
+    //TODO: set up fix tp location
     loop {
+        println!("Remember you can right click to the prompt to paste your latest copy entry");
+        println!("Add person's coordinates. E.g. Kay Memoryland(17.5 , 46.3)");
+        println!("Delete by typing just the name. E.g. Kay");
+        //TODO: decide when and where the node calculation should be
         let mut input = String::new();
         io::stdin().read_line(&mut input)
         .expect("Error reading input");
 
-        println!("{}", input);
+        println!("{:?}", input);
+        //TODO: add entries in Map/dictionary
+        let mut _splitted: &Vec<String> = &input.splitn(2," ").map(|s| s.to_string()).collect();
+        println!("{:?}", _splitted);
+        opened_maps.insert(_splitted[0].clone(), _splitted[1].clone());
+        println!("{:?}",opened_maps);
         if input.trim() == "end" {
             println!("See ya!");
             break;
         }
     }
 }
+// Yak T'el ( 16.6  , 12.4 )
+// Yak T'el ( 12.3  , 12.3 )
